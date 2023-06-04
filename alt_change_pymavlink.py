2de4c9f1ba90147377d26571305a272ef5e766b3
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-06-04 11:08:25 +0300

    pymavlink code for UAV

---
+++ alt_change_pymavlink.py
@@ -0,0 +1,70 @@
+from pymavlink import mavutil
+import time
+
+# Connect to the simulated drone
+connection_string = 'tcp:127.0.0.1:5760'
+mav = mavutil.mavlink_connection(connection_string)
+
+# Print welcome message
+print("Drone Playground Simulation")
+print("Prepare for takeoff!")
+
+# Set the vehicle mode to GUIDED
+mav.mav.set_mode_send(
+    mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, 
+    mavutil.mavlink.MAV_MODE_GUIDED)
+
+# Arm the vehicle
+mav.mav.command_long_send(
+    mav.target_system, 
+    mav.target_component, 
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 
+    0, 1, 0, 0, 0, 0, 0, 0)
+
+# Take off to a desired height
+target_altitude = 15  # Desired altitude in meters
+print("Taking off...")
+mav.mav.command_long_send(
+    mav.target_system, 
+    mav.target_component, 
+    mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 
+    0, 0, 0, 0, 0, 0, 0, target_altitude)
+
+# Ascend and provide altitude updates
+while True:
+    msg = mav.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+    altitude = msg.alt / 1000.0  # Altitude is in millimeters
+    print("Ascending - Current altitude: %.2f meters" % altitude)
+    if altitude >= target_altitude * 0.95:
+        print("Reached the desired height of %d meters!" % target_altitude)
+        break
+    time.sleep(1)
+
+# Descend and land
+print("Descending and landing...")
+mav.mav.command_long_send(
+    mav.target_system, 
+    mav.target_component, 
+    mavutil.mavlink.MAV_CMD_NAV_LAND, 
+    0, 0, 0, 0, 0, 0, 0, 0)
+
+# Wait until the vehicle has landed
+while True:
+    msg = mav.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+    altitude = msg.alt / 1000.0  # Altitude is in millimeters
+    if altitude <= 0.2:  # Assuming landing is complete when altitude is close to 0
+        print("Drone has landed successfully!")
+        break
+    print("Waiting for vehicle to land...")
+    time.sleep(1)
+
+# Disarm and close connection
+mav.mav.command_long_send(
+    mav.target_system, 
+    mav.target_component, 
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 
+    0, 0, 0, 0, 0, 0, 0, 0)
+mav.close()
+
+# Shutdown complete
+print("Turning off the drone playground. Goodbye!")
