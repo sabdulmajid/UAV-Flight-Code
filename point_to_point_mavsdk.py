2fff6e76400a5e4485dfd71f87305793027bcf23
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-06-06 11:17:38 +0300

    checking if armed or not

--- point_to_point_mavsdk.py
+++ point_to_point_mavsdk.py
@@ -7,3 +7,8 @@ async def run():
     drone = System()
     await drone.connect(system_address="udp://:14540")
 
+    # Check if the drone is ready for flight
+    async for is_armed in drone.telemetry.armed():
+        if not is_armed:
+            print("Drone is not armed. Please arm the drone before running this script.")
+            return
