eda32cc3c306a1230cbbcd433eede8a25befd27d
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-06-13 11:24:01 +0300

    dronekit code for first test flight

---
+++ Simple Takeoff and Landing/takeoff_and_land_dronekit.py
@@ -0,0 +1,48 @@
+import time
+from dronekit import connect, VehicleMode
+
+# Connect to the simulated drone playground
+vehicle = connect('/dev/ttyAMA0', wait_ready=True)
+
+# Print welcome message
+print("Drone Takeoff Code")
+print("Prepare for takeoff!")
+
+# Set the vehicle mode to GUIDED
+vehicle.mode = VehicleMode("GUIDED")
+
+# Arm the vehicle
+vehicle.armed = True
+
+# Take off to a desired height
+target_altitude = 1  # Desired altitude in meters
+print("Taking off...")
+vehicle.simple_takeoff(target_altitude)
+
+# Ascend and provide altitude updates
+while vehicle.location.global_relative_frame.alt < target_altitude * 0.95:
+    altitude = vehicle.location.global_relative_frame.alt
+    print("Ascending - Current altitude: %.2f meters" % altitude)
+    time.sleep(1)
+
+# Reached the desired height
+print("Reached the desired height of %d meters!" % target_altitude)
+
+# Descend and land
+print("Descending and landing...")
+vehicle.mode = VehicleMode("LAND")
+
+# Wait until the vehicle has landed
+while not vehicle.is_armable:
+    print("Waiting for vehicle to land...")
+    time.sleep(1)
+
+# Drone has landed
+print("Drone has landed successfully!")
+
+# Disarm and close connection
+vehicle.armed = False
+vehicle.close()
+
+# Shutdown complete
+print("Turning off the drone. Goodbye!")
