6eeb3cea2945acd67f28efcc897981ab924b4d04
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-06-18 13:43:32 +0300

    sensor guide added for ease

--- Simple Takeoff and Landing/dronekit_sensorInfo_takeoff.py
+++ Simple Takeoff and Landing/dronekit_sensorInfo_takeoff.py
@@ -54,3 +54,46 @@ vehicle.close()
 
 # Shutdown complete
 print("Turning off the drone. Goodbye!")
+
+"""
+All different types of sensors:
+
+while vehicle.location.global_relative_frame.alt < target_altitude * 0.95:
+    try:
+        # Retrieve sensor data
+        altitude = vehicle.location.global_relative_frame.alt
+        latitude = vehicle.location.global_relative_frame.lat
+        longitude = vehicle.location.global_relative_frame.lon
+        roll = vehicle.attitude.roll
+        pitch = vehicle.attitude.pitch
+        yaw = vehicle.attitude.yaw
+        voltage = vehicle.battery.voltage
+        current = vehicle.battery.current
+        remaining_capacity = vehicle.battery.remaining
+        ground_speed = vehicle.velocity[0]
+        vertical_speed = vehicle.velocity[2]
+        heading = vehicle.heading
+        armed_status = vehicle.armed
+        flight_mode = vehicle.mode.name
+
+        # Calculate battery percentage
+        full_capacity_voltage = 12.6  # Adjust this value according to your battery specifications
+        empty_capacity_voltage = 10.6  # Adjust this value according to your battery specifications
+        battery_percentage = ((voltage - empty_capacity_voltage) / (full_capacity_voltage - empty_capacity_voltage)) * 100
+        battery_percentage = max(0, min(100, battery_percentage))  # Ensure the percentage is between 0 and 100
+
+        # Display sensor data
+        print("Ascending - Current altitude: %.2f meters" % altitude)
+        print("Latitude: %.6f, Longitude: %.6f" % (latitude, longitude))
+        print("Roll: %.2f, Pitch: %.2f, Yaw: %.2f" % (roll, pitch, yaw))
+        print("Battery Percentage: %.2f%%" % battery_percentage)
+        print("Ground Speed: %.2fm/s, Vertical Speed: %.2fm/s" % (ground_speed, vertical_speed))
+        print("Heading: %.2f" % heading)
+        print("Armed Status: %s, Flight Mode: %s" % (armed_status, flight_mode))
+
+        time.sleep(1)
+    except Exception as e:
+        print("Error retrieving sensor data:", str(e))
+
+
+"""
