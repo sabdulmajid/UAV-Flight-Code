89803ae5566bda6bb11b39bbad364185d88a1cc8
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-06-18 12:16:15 +0300

    arming the drone and setting to guided mode

--- Simple Takeoff and Landing/dronekit_sensorInfo_takeoff.py
+++ Simple Takeoff and Landing/dronekit_sensorInfo_takeoff.py
@@ -4,3 +4,12 @@ from dronekit import connect, VehicleMode
 # Connect to the simulated drone playground
 vehicle = connect('/dev/ttyAMA0', wait_ready=True)  # /dev/ttyS0
 
+# Print welcome message
+print("Drone Takeoff Code")
+print("Prepare for takeoff!")
+
+# Set the vehicle mode to GUIDED
+vehicle.mode = VehicleMode("GUIDED")
+
+# Arm the vehicle
+vehicle.armed = True
