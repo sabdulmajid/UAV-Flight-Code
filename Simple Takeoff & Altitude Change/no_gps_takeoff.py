676270dd359f7c1a6aaf6aea2a6596846e6210ef
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-06-20 13:14:46 +0300

    changes to code to test in NO_GPS setting

--- Simple Takeoff & Altitude Change/no_gps_takeoff.py
+++ Simple Takeoff & Altitude Change/no_gps_takeoff.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python
 
 """
-
 set_attitude_target.py: (Copter Only)
-
 This example shows how to move/direct Copter and send commands
- in GUIDED_NOGPS mode using DroneKit Python.
-
+in GUIDED_NOGPS mode using DroneKit Python.
 Caution: A lot of unexpected behaviors may occur in GUIDED_NOGPS mode.
-        Always watch the drone movement, and make sure that you are in dangerless environment.
-        Land the drone as soon as possible when it shows any unexpected behavior.
-
+Always watch the drone movement, and make sure that you are in a dangerless environment.
+Land the drone as soon as possible when it shows any unexpected behavior.
 Tested in Python 2.7.10
-
 """
 
-from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
-from pymavlink import mavutil # Needed for command message definitions
+from dronekit import connect, VehicleMode
+from pymavlink import mavutil
 import time
 import math
 
 # Set up option parsing to get connection string
 import argparse
-parser = argparse.ArgumentParser(description='Control Copter and send commands in GUIDED mode ')
-parser.add_argument('--connect',
-                   help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+
+parser = argparse.ArgumentParser(description='Control Copter and send commands in GUIDED mode')
+parser.add_argument('--connect', help="Vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
 
 connection_string = args.connect
@@ -33,13 +28,14 @@ sitl = None
 # Start SITL if no connection string specified
 if not connection_string:
     import dronekit_sitl
+
     sitl = dronekit_sitl.start_default()
     connection_string = sitl.connection_string()
-
 
 # Connect to the Vehicle
 print('Connecting to vehicle on: %s' % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
+
 
 def arm_and_takeoff_nogps(aTargetAltitude):
     """
@@ -52,10 +48,8 @@ def arm_and_takeoff_nogps(aTargetAltitude):
 
     print("Basic pre-arm checks")
     # Don't let the user try to arm until autopilot is ready
-    # If you need to disable the arming check,
-    # just comment it with your own responsibility.
     while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
+        print("Waiting for vehicle to initialize...")
         time.sleep(1)
 
     print("Arming motors")
@@ -65,7 +59,7 @@ def arm_and_takeoff_nogps(aTargetAltitude):
     vehicle.armed = True
 
     while not vehicle.armed:
-        print(" Waiting for arming...")
+        print("Waiting for arming...")
         vehicle.armed = True
         time.sleep(1)
 
@@ -74,19 +68,19 @@ def arm_and_takeoff_nogps(aTargetAltitude):
     thrust = DEFAULT_TAKEOFF_THRUST
     while True:
         current_altitude = vehicle.location.global_relative_frame.alt
-        print(" Altitude: %f  Desired: %f" %
-              (current_altitude, aTargetAltitude))
-        if current_altitude >= aTargetAltitude*0.95: # Trigger just below target alt.
+        print("Altitude: %f  Desired: %f" % (current_altitude, aTargetAltitude))
+        if current_altitude >= aTargetAltitude * 0.95:  # Trigger just below target alt.
             print("Reached target altitude")
             break
-        elif current_altitude >= aTargetAltitude*0.6:
+        elif current_altitude >= aTargetAltitude * 0.6:
             thrust = SMOOTH_TAKEOFF_THRUST
-        set_attitude(thrust = thrust)
+        send_attitude_target(thrust=thrust)
         time.sleep(0.2)
 
-def send_attitude_target(roll_angle = 0.0, pitch_angle = 0.0,
-                         yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,
-                         thrust = 0.5):
+
+def send_attitude_target(roll_angle=0.0, pitch_angle=0.0,
+                         yaw_angle=None, yaw_rate=0.0, use_yaw_rate=False,
+                         thrust=0.5):
     """
     use_yaw_rate: the yaw can be controlled using yaw_angle OR yaw_rate.
                   When one is used, the other is ignored by Ardupilot.
@@ -101,21 +95,22 @@ def send_attitude_target(roll_angle = 0.0, pitch_angle = 0.0,
     # Thrust == 0.5: Hold the altitude
     # Thrust <  0.5: Descend
     msg = vehicle.message_factory.set_attitude_target_encode(
-        0, # time_boot_ms
-        1, # Target system
-        1, # Target component
+        0,  # time_boot_ms
+        1,  # Target system
+        1,  # Target component
         0b00000000 if use_yaw_rate else 0b00000100,
-        to_quaternion(roll_angle, pitch_angle, yaw_angle), # Quaternion
-        0, # Body roll rate in radian
-        0, # Body pitch rate in radian
-        math.radians(yaw_rate), # Body yaw rate in radian/second
+        to_quaternion(roll_angle, pitch_angle, yaw_angle),  # Quaternion
+        0,  # Body roll rate in radian
+        0,  # Body pitch rate in radian
+        math.radians(yaw_rate),  # Body yaw rate in radian/second
         thrust  # Thrust
     )
     vehicle.send_mavlink(msg)
 
-def set_attitude(roll_angle = 0.0, pitch_angle = 0.0,
-                 yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,
-                 thrust = 0.5, duration = 0):
+
+def set_attitude(roll_angle=0.0, pitch_angle=0.0,
+                 yaw_angle=None, yaw_rate=0.0, use_yaw_rate=False,
+                 thrust=0.5, duration=0):
     """
     Note that from AC3.3 the message should be re-sent more often than every
     second, as an ATTITUDE_TARGET order has a timeout of 1s.
@@ -137,8 +132,9 @@ def set_attitude(roll_angle = 0.0, pitch_angle = 0.0,
                          0, 0, True,
                          thrust)
 
+
 # Some fancy math conversions to convert degrees to quaternions
-def to_quaternion(roll = 0.0, pitch = 0.0, yaw = 0.0):
+def to_quaternion(roll=0.0, pitch=0.0, yaw=0.0):
     """
     Convert degrees to quaternions
     """
@@ -156,17 +152,18 @@ def to_quaternion(roll = 0.0, pitch = 0.0, yaw = 0.0):
 
     return [w, x, y, z]
 
+
 # Take off 1 meter in GUIDED_NOGPS mode.
 arm_and_takeoff_nogps(1)
 
 # Hold the position for 5 seconds.
 print("Hold position for 5 seconds")
-set_attitude(duration = 5)
+set_attitude(duration=5)
 
 # Uncomment the lines below for testing roll angle and yaw rate.
 # Make sure that there is enough space for testing this.
 
-set_attitude(roll_angle = 1, thrust = 0.5, duration = 3)
+set_attitude(roll_angle=1, thrust=0.5, duration=3)
 set_attitude(yaw_rate = 30, thrust = 0.5, duration = 3)
 
 # Move the drone forward and backward.
