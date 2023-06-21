2919eaa2fd7257b9d31bf7e594210990bd02c215
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-06-21 10:37:44 +0300

    fixed buggy code for first NEWS test, and added code for new NEWS test

--- Point To Point Code/NEWS_direction_2.py
+++ Point To Point Code/NEWS_direction_2.py
@@ -1,8 +1,7 @@
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 from pymavlink import mavutil
 import time
-import argparse
-
+import argparse  
 parser = argparse.ArgumentParser()
 parser.add_argument('--connect', default='/dev/ttyAMA0')
 args = parser.parse_args()
@@ -10,7 +9,7 @@ args = parser.parse_args()
 # Connect to the Vehicle
 print ('Connecting to vehicle on: %s') % args.connect
 vehicle = connect(args.connect, baud=57600, wait_ready=True)
-# 921600 is the baudrate that you have set in Mission Planner or QGC
+#921600 is the baudrate that you have set in the mission plannar or qgc
 
 # Function to arm and then takeoff to a user specified altitude
 def arm_and_takeoff(aTargetAltitude):
@@ -36,7 +35,7 @@ def arm_and_takeoff(aTargetAltitude):
   # Check that vehicle has reached takeoff altitude
   while True:
     print (" Altitude: ", vehicle.location.global_relative_frame.alt) 
-    # Break and return from function just below target altitude.        
+    #Break and return from function just below target altitude.        
     if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: 
       print ("Reached target altitude")
       break
@@ -56,37 +55,46 @@ def set_velocity_body(Vx,Vy,Vz):
   vehicle.send_mavlink(msg)
   vehicle.flush()
 
-arm_and_takeoff(2) # Take off to target altitude of 2 meters
+# Initialize the takeoff sequence to 2m
+arm_and_takeoff(2)
 
 counter=0
-while counter<=2:
-  set_velocity_body(1,0,0)
-  print("Direction: North relative to heading of drone")
-  time.sleep(1)
-  counter=counter+1
-
-counter=0
-while counter<=2:
-  set_velocity_body(-1,0,0)
-  print("Direction: South relative to heading of drone")
-  time.sleep(1)
-  counter=counter+1
+while counter<2:
+    set_velocity_body(1,0,0)
+    print("Direction: North relative to heading of drone")
+    time.sleep(1)
+    counter=counter+1
+  
+time.sleep(1)
   
 counter=0
-while counter<=2:
-  set_velocity_body(0,1,0)
-  print("Direction: east relative to heading of drone")
-  time.sleep(1)
-  counter=counter+1
+while counter<2:
+    set_velocity_body(-1,0,0)
+    print("Direction: South relative to heading of drone")
+    time.sleep(1)
+    counter=counter+1
+
+time.sleep(1)
 
 counter=0
-while counter<=2:
-  set_velocity_body(0,-1,0)
-  print("Direction: west relative to heading of drone")
-  time.sleep(1)
-  counter=counter+1
+while counter<2:
+    set_velocity_body(0,1,0)
+    print("Direction: east relative to heading of drone")
+    time.sleep(1)
+    counter=counter+1
+ 
+time.sleep(1)
 
-  
+counter=0
+while counter<2:
+    set_velocity_body(0,-1,0)
+    print("Direction: west relative to heading of drone")
+    time.sleep(1)
+    counter=counter+1
+
+time.sleep(1)
+
+
 print("Setting mode to LAND and decreasing altitude to 0m")
 vehicle.mode = VehicleMode("LAND")
 
