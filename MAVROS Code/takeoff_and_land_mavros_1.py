51e4979f8507e1b83878f247d404187f10082dec
sabdulmajid <ayman.hasib@outlook.com>
2023-06-24 19:44:20 +0600

    main function (preset to 2m height in altitude)

--- MAVROS Code/takeoff_and_land_mavros_1.py
+++ MAVROS Code/takeoff_and_land_mavros_1.py
@@ -56,3 +56,43 @@ def land():
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
 
+# Main function
+if __name__ == '__main__':
+    rospy.init_node('quadcopter_control')
+
+    # Subscribe to pose and velocity topics
+    rospy.Subscriber('/mavros/local_position/pose', PoseStamped, pose_callback)
+    rospy.Subscriber('/mavros/local_position/velocity', TwistStamped, velocity_callback)
+
+    # Set the rate (in Hz) at which to publish messages
+    rate = rospy.Rate(20)
+
+    # Wait for the connection to be established
+    while current_pose.header.seq == 0 or current_velocity.header.seq == 0:
+        rate.sleep()
+
+    # Arm the quadcopter
+    arm()
+
+    # Set the mode to GUIDED
+    set_mode('GUIDED')
+
+    # Takeoff to a specified altitude
+    takeoff(2.0)  # Replace with your desired takeoff altitude (in meters)
+
+    # Wait for the quadcopter to reach the desired altitude
+    while current_pose.pose.position.z < 1.9:  # Replace with your desired altitude minus a tolerance value
+        rate.sleep()
+
+    # Descend back down
+    set_mode('LAND')
+
+    # Wait for the quadcopter to land
+    while current_pose.pose.position.z > 0.1:  # Replace with your desired ground clearance (in meters)
+        rate.sleep()
+
+    # Disarm the quadcopter
+    set_mode('STABILIZE')
+    arm()
+
+    rospy.spin()
