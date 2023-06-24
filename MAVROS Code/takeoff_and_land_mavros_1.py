ff2c73ad12cd2ff21ce25656a358b7377e3f3ea1
sabdulmajid <ayman.hasib@outlook.com>
2023-06-24 19:42:19 +0600

    functions for position & velocity

--- MAVROS Code/takeoff_and_land_mavros_1.py
+++ MAVROS Code/takeoff_and_land_mavros_1.py
@@ -6,3 +6,17 @@ from mavros_msgs.srv import CommandTOL, SetMode
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from std_msgs.msg import Header
 
+# Global variables
+current_pose = PoseStamped()
+current_velocity = TwistStamped()
+
+# Callback function for the current position
+def pose_callback(msg):
+    global current_pose
+    current_pose = msg
+
+# Callback function for the current velocity
+def velocity_callback(msg):
+    global current_velocity
+    current_velocity = msg
+
