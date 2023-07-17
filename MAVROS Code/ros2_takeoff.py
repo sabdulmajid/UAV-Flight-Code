7a8e35e66c406f514f05f1ffe9e81d342382440f
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-07-17 11:45:33 +0300

    advertise and set rate

--- MAVROS Code/ros2_takeoff.py
+++ MAVROS Code/ros2_takeoff.py
@@ -17,3 +17,12 @@ class QuadcopterControl(Node):
         # Subscribe to pose and velocity topics
         self.pose_sub = self.create_subscription(PoseStamped, '/mavros/local_position/pose', self.pose_callback, 10)
         self.velocity_sub = self.create_subscription(TwistStamped, '/mavros/local_position/velocity', self.velocity_callback, 10)
+
+        # Advertise to the necessary topics
+        self.arm_pub = self.create_publisher(CommandBool, '/mavros/cmd/arming', 10)
+        self.mode_pub = self.create_publisher(SetMode, '/mavros/set_mode', 10)
+        self.takeoff_pub = self.create_publisher(CommandTOL, '/mavros/cmd/takeoff', 10)
+        self.land_pub = self.create_publisher(CommandTOL, '/mavros/cmd/land', 10)
+
+        # Set the rate (in Hz) at which to publish messages
+        self.rate = self.create_rate(20)
