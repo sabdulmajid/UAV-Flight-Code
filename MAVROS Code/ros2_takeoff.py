32714b96f019c7898ad8276ebcc1bc8e7a2cdeba
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-07-17 11:44:53 +0300

    global variables

--- MAVROS Code/ros2_takeoff.py
+++ MAVROS Code/ros2_takeoff.py
@@ -5,3 +5,11 @@ from rclpy.node import Node
 from mavros_msgs.msg import CommandBool, CommandTOL, SetMode
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from std_msgs.msg import Header
+
+class QuadcopterControl(Node):
+    def __init__(self):
+        super().__init__('quadcopter_control')
+
+        # Global variables
+        self.current_pose = PoseStamped()
+        self.current_velocity = TwistStamped()
