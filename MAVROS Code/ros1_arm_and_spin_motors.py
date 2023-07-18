9a42e8c2941db119bd09748d269eb83596417ab8
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-07-18 12:11:10 +0300

    changes to arming code to fix errors

--- MAVROS Code/ros1_arm_and_spin_motors.py
+++ MAVROS Code/ros1_arm_and_spin_motors.py
@@ -13,14 +13,15 @@ def arm_and_spin_motors():
 
     mode = "GUIDED"
 
-    arming_client(True)
+    arming_result = arming_client(True)
 
     while not rospy.is_shutdown():
-        if arming_client().success:
+        if arming_result.success:
             rospy.loginfo("Copter armed successfully!")
             break
         rospy.loginfo("Arming failed. Retrying...")
         rospy.sleep(1)
+
 
     set_mode_client(base_mode=0, custom_mode=mode)
 
