4df28a727f893b7180241c559702e5e123d5c0b8
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2023-07-18 13:00:32 +0300

    changing mode to STABILIZE for indoor connection

--- MAVROS Code/ros1_arm_and_spin_motors.py
+++ MAVROS Code/ros1_arm_and_spin_motors.py
@@ -11,8 +11,9 @@ def arm_and_spin_motors():
     arming_client = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
     set_mode_client = rospy.ServiceProxy('/mavros/set_mode', SetMode)
 
-    mode = "GUIDED"
+    mode = "STABILIZE"
 
+    # Arm the vehicle
     arming_result = arming_client(True)
 
     while not rospy.is_shutdown():
@@ -22,18 +23,23 @@ def arm_and_spin_motors():
         rospy.loginfo("Arming failed. Retrying...")
         rospy.sleep(1)
 
-
+    # Set the mode to STABILIZE
     set_mode_client(base_mode=0, custom_mode=mode)
 
-    actuator_pub = rospy.Publisher('/mavros/setpoint_raw/actuator_control', ActuatorControl, queue_size=10)
-    rospy.sleep(1)  
+    rospy.sleep(1)
 
+    # Spin the motors
+    actuator_pub = rospy.Publisher('/mavros/setpoint_raw/actuator_control', ActuatorControl, queue_size=10)
     actuator_msg = ActuatorControl()
     actuator_msg.header.stamp = rospy.Time.now()
     actuator_msg.group_mix = 0
-    actuator_msg.controls = [0, 0, 0, 0.1, 0.1, 0, 0, 0]  # Adjust motor speeds here
+    actuator_msg.controls = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]  # Maximum throttle to spin motors
 
-    actuator_pub.publish(actuator_msg)
+    rate = rospy.Rate(10)  # Publish at 10Hz
+
+    for _ in range(50):  # Publish for 5 seconds
+        actuator_pub.publish(actuator_msg)
+        rate.sleep()
 
     rospy.loginfo("Motors spinning!")
 
