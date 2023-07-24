e49081c84d7ecce11f81f7bf4d2b449911bda976
sabdulmajid <ayman.hasib@outlook.com>
2023-07-24 13:30:31 -0400

    making yolo function (pretrained model)

--- Object Detection Code/object_detection.py
+++ Object Detection Code/object_detection.py
@@ -1,3 +1,11 @@
 import cv2
 import numpy as np
 
+def load_yolo():
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    classes = []
+    with open("coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    layers_names = net.getLayerNames()
+    output_layers = [layers_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    return net, classes, output_layers
