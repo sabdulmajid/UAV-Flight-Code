0e2d9ee3bd8e60be2630ebaddbb84e9d3e82a538
sabdulmajid <ayman.hasib@outlook.com>
2023-07-24 13:31:27 -0400

    created loop for detection of objects

--- Object Detection Code/object_detection.py
+++ Object Detection Code/object_detection.py
@@ -19,3 +19,21 @@ def detect_objects(img, net, output_layers, classes):
     class_ids = []
     confidences = []
     boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
